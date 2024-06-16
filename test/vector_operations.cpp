5aa42459064c79ff65172e6da5aaa85dacda803c
naushada <naushad.dln@gmail.com>
2024-06-16 15:44:35 +0530

    Aded Test Case for Matrix Multiplication for MmX1

--- test/vector_operations.cpp
+++ test/vector_operations.cpp
@@ -21,9 +21,6 @@ void VectorOperations::TestBody() {
 
 }
 
-
-
-
 TEST_F(VectorOperations, VectorMultiplication_M1x3) {
     Vector first = {
         {1}, 
@@ -42,6 +39,154 @@ TEST_F(VectorOperations, VectorMultiplication_M1x3) {
     EXPECT_EQ(result, 4);
 }
 
+TEST_F(VectorOperations, VectorMultiplication_M1x4) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2},
+        {4}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1},
+        {5}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r().T() * space()->s();
+    EXPECT_EQ(result, 24);
+}
 
+TEST_F(VectorOperations, VectorMultiplication_M1x5) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2},
+        {4},
+        {3}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1},
+        {5},
+        {3}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r().T() * space()->s();
+    EXPECT_EQ(result, 33);
+}
+
+TEST_F(VectorOperations, VectorMultiplication_M1x6) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2},
+        {4},
+        {3},
+        {2}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1},
+        {5},
+        {3},
+        {2}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r().T() * space()->s();
+    EXPECT_EQ(result, 37);
+}
+
+TEST_F(VectorOperations, VectorMultiplication_M3x1) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r() * space()->s().T();
+    EXPECT_EQ(result, 4);
+}
+
+TEST_F(VectorOperations, VectorMultiplication_M4x1) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2},
+        {4}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1},
+        {5}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r() * space()->s().T();
+    EXPECT_EQ(result, 24);
+}
+
+TEST_F(VectorOperations, VectorMultiplication_M5x1) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2},
+        {4},
+        {3}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1},
+        {5},
+        {3}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r() * space()->s().T();
+    EXPECT_EQ(result, 33);
+}
+
+TEST_F(VectorOperations, VectorMultiplication_M6x1) {
+    Vector first = {
+        {1}, 
+        {-1},
+        {2},
+        {4},
+        {3},
+        {2}
+    };
+    Vector second = {
+        {2}, 
+        {0},
+        {1},
+        {5},
+        {3},
+        {2}
+    };
+    /// @brief vectorSpace is aset of vectors
+    space()->r(first);
+    space()->s(second);
+    auto result = space()->r() * space()->s().T();
+    EXPECT_EQ(result, 37);
+}
 
 #endif /*__vector_operations_cpp__*/
